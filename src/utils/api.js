d278a27c3090e53f63885749e660285d3aa908e9
leoTechHub <godswilltrade@gmail.com>
2026-02-12 00:58:05 +0100

    all done

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 import toast from "react-hot-toast";
 
-const API_BASE_URL = "http://localhost:3000/api";
+const API_BASE_URL = "https://nutritionbackend-murex.vercel.app/api";
 const PYTHON_API_URL = "http://localhost:5000/api";
 
 // Node.js API instance
